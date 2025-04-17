3872ebf2bd1d5d8c0c810e2dc437d9a11b39ab57
nandoooooooo <nando.schuermann@getabstract.com>
2025-04-17 17:44:55 +0200

    Third Test Case written

--- src/payroll.test.ts
+++ src/payroll.test.ts
@@ -49,3 +49,31 @@ describe("a 18 year old apprentice with a monthly salary of 1200 should have AHV
         expect(payslip.net).toBeCloseTo(salary.gross - (ahv + iv + eo + alv + nbu))
     })
 })
+
+describe("a 21 year old employee with a monthly salary of 5900 should have AHV, IV, EO, ALV, NBU and PK deducations", () => {
+
+    test("6 deducations", () => {
+        // Arrange
+
+        const today = new Date()
+        const salary: Salary = {
+            born: new Date(today.getFullYear()-21, today.getMonth(), today.getDate()),
+            payday: new Date(),
+            gross: 5900,
+        }
+
+        // Act
+        const ahv = salary.gross * (8.7 /100)
+        const iv = salary.gross * (1.4 / 100)
+        const eo = salary.gross * (0.5 / 100)
+        const alv = salary.gross * (1.1 / 100)
+        const nbu = salary.gross * (0.73 / 100)
+        const pk = salary.gross * (8.9 / 100)
+        const payslip = calculatePayslip(salary)
+
+        // Assert
+        expect(payslip.deductions.size).toBe(6)
+        expect(payslip.totalDeductions).toBeCloseTo(ahv + iv + eo + alv + nbu + pk)
+        expect(payslip.net).toBeCloseTo(salary.gross - (ahv + iv + eo + alv + nbu + pk))
+    })
+})
